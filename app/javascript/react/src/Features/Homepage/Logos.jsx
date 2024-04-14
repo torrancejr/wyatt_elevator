aead361604eb4512c7633230940a9630cc8d153e
Ryan <torrance.jr@gmail.com>
2024-04-14 15:35:48 -0400

    changed text below logos

--- app/javascript/react/src/Features/Homepage/Logos.jsx
+++ app/javascript/react/src/Features/Homepage/Logos.jsx
@@ -43,7 +43,7 @@ export default function Logos() {
                 </div>
                 <div className="mt-16 flex justify-center">
                     <p className="relative rounded-full bg-gray-50 px-4 py-1.5 text-sm leading-6 text-gray-600 ring-1 ring-inset ring-gray-900/5">
-                        <span className="hidden md:inline">Over 2500 companies use our tools to better their business.</span>
+                        <span className="hidden md:inline">Over 1500 companies use our services to keep things operating smoothly.</span>
                         <a href="#" className="font-semibold text-green-600">
                             <span className="absolute inset-0" aria-hidden="true" /> Read our customer stories{' '}
                             <span aria-hidden="true">&rarr;</span>
