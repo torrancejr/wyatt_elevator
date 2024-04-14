5e4b97ed800e5ce47acc04aaf012911598c6ea99
Ryan <torrance.jr@gmail.com>
2024-04-14 11:14:26 -0400

    hero done

--- app/javascript/react/src/Features/Header.jsx
+++ app/javascript/react/src/Features/Header.jsx
@@ -151,7 +151,7 @@ export default function Example() {
                                             href="#"
                                             className="rounded-md bg-green-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
                                         >
-                                            Get started
+                                            Contact Us
                                         </a>
                                         <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
                                             Service Area <span aria-hidden="true">→</span>
