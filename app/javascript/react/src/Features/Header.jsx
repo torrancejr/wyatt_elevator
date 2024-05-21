afbb074ac8ffbbccdb345cca0aa81501300f2bbb
Ryan <torrance.jr@gmail.com>
2024-05-21 10:22:21 -0400

    fixed logo height

--- app/javascript/react/src/Features/Header.jsx
+++ app/javascript/react/src/Features/Header.jsx
@@ -37,10 +37,10 @@ export default function Example() {
             <header className="absolute inset-x-0 top-0 z-50">
                 <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
                     <div className="flex lg:flex-1">
-                        <a href="#" className="-m-1.5 p-1.5">
+                        <a href="/" className="-m-1.5 p-1.5">
                             <span className="sr-only">Wyatt Elevator</span>
                             <img
-                                className="h-8 w-auto"
+                                className="h-16 w-auto"
                                 src="https://wyatt-elevator.s3.amazonaws.com/wyattlogo-sm.jpg"
                                 alt=""
                             />
