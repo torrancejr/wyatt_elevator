11e20899fca48eadd83c6510fb755b04f9350605
Ryan <torrance.jr@gmail.com>
2024-05-28 15:39:13 -0400

    renamed with .jsx extension

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Entry point for the build script in your package.json
 // import "@hotwired/turbo-rails"
 import "./controllers"
-import "./react/src/components/App"
+import "./react/src/components/App.jsx"
 // import "./react/src/components"
 import "./stylesheets/application.css"
