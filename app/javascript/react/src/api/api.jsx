fca3ab7dbd19a566b996cd69ecf71edd45d2abc8
Ryan <torrance.jr@gmail.com>
2024-05-28 17:24:55 -0400

    heroku base for axios

--- app/javascript/react/src/api/api.jsx
+++ app/javascript/react/src/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/',
+    baseURL: 'https://wyatt-53e54f3152e0.herokuapp.com/',
     headers: {
         'Content-Type': 'application/json'
     }
