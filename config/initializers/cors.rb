c88597077667736b0a3c819c9c17f2d9f948e210
Ryan <torrance.jr@gmail.com>
2024-05-28 19:08:36 -0400

    base url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*' # later change to the domain of the frontend app
+    origins `<%= ENV['BASE_URL']%>` # later change to the domain of the frontend app
     resource '*',
              headers: :any,
              methods: %i[get post put patch delete options head],
