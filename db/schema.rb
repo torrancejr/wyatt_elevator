195a930c65902c4b9b336834b22670818d74b64b
Ryan <torrance.jr@gmail.com>
2024-05-28 13:46:59 -0400

    switched to pg lets check deploy

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_05_23_173146) do
-  create_table "inquiries", charset: "utf8mb4", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "inquiries", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.string "location"
@@ -22,7 +25,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_23_173146) do
     t.string "phone_number"
   end
 
-  create_table "searches", charset: "utf8mb4", force: :cascade do |t|
+  create_table "searches", force: :cascade do |t|
     t.string "location"
     t.float "latitude"
     t.float "longitude"
@@ -30,7 +33,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_23_173146) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
